c0cefcbfd027bcd2bac9a1cb9013727457789888
Marcellsantoso <santoso.marcellinus@gmail.com>
2021-08-10 23:11:26 +0700

    personal page
    Completed : personal
    Note : text stylings, button styling & handler
    Next : Schedule Video

--- lib/Page/Personal.dart
+++ lib/Page/Personal.dart
@@ -10,6 +10,15 @@ class Personal extends StatefulWidget {
 }
 
 class _PersonalState extends State<Personal> {
+  List<String> dataGoal = ['Savings', 'Retirement funds', 'House', 'New phone'];
+  List<String> dataIncome = ['Less than Rp. 10.000.000', 'Rp. 10.000.000 - Rp. 20.000.000', 'Rp. 20.000.000 - Rp. 50.000.000', 'More than Rp. 50.000.000'];
+  List<String> dataExpense = ['Less than Rp. 5.000.000', 'Rp. 5.000.000 - Rp. 10.000.000', 'Rp. 10.000.000 - Rp. 20.000.000', 'More than Rp. 20.000.000'];
+
+  String selectedGoal = "";
+  String selectedIncome = "";
+  String selectedExpense = "";
+  bool enabledNext = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +29,8 @@ class _PersonalState extends State<Personal> {
   }
 
   Widget body() {
+    enabledNext =
+        selectedGoal != "" && selectedIncome != "" && selectedExpense != "";
     return CustomScrollView(slivers: [
       SliverFillRemaining(
           child: Container(
@@ -37,6 +48,36 @@ class _PersonalState extends State<Personal> {
             "Please fill in the information below and your goal for digital saving.",
             style: textWhite,
           ),
+          SizedBox(height: padding),
+          dropdown(
+              title: "Goal for activation",
+              data: dataGoal,
+              selected: selectedGoal,
+              handler: (selected) {
+                setState(() {
+                  selectedGoal = selected;
+                });
+              }),
+          SizedBox(height: padding),
+          dropdown(
+              title: "Monthly income",
+              data: dataIncome,
+              selected: selectedIncome,
+              handler: (selected) {
+                setState(() {
+                  selectedIncome = selected;
+                });
+              }),
+          SizedBox(height: padding),
+          dropdown(
+              title: "Monthly expense",
+              data: dataExpense,
+              selected: selectedExpense,
+              handler: (selected) {
+                setState(() {
+                  selectedExpense = selected;
+                });
+              }),
           Spacer(),
           SizedBox(
             width: double.infinity,
@@ -44,11 +85,41 @@ class _PersonalState extends State<Personal> {
               text: "Next",
               backgroundColor: Colors.white,
               textColor: colorPrimary,
+              handler: enabledNext ? () => nextPage() : null,
             ),
           ),
           SizedBox(height: padding),
         ]),
       ))
     ]);
+  }
+
+  Container dropdown({title, List<String>? data, selected = "", handler}) {
+    if (data == null) return Container();
+    return Container(
+        padding: EdgeInsets.only(left: padding, right: padding, top: padding_s),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(radius)),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text("$title", style: textInfoSmall),
+          DropdownButton<String>(
+            items: data.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            isExpanded: true,
+            value: selected != "" ? selected : null,
+            onChanged: (val) {
+              handler(val);
+            },
+            hint: Text("- Choose Option -", style: text),
+            underline: Container(),
+          ),
+        ]));
+  }
+
+  void nextPage() {
   }
 }
